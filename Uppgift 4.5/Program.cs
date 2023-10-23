1505b73bf48e1f292e2ecb79f2bdb62675a82d40
Dijar06 <dize06001@utb.vaxjo.se>
2023-10-23 15:00:39 +0200

    Add project files.

---
+++ Uppgift 4.5/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Uppgift4._5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Skriv in ett heltal");
+            string heltal = Console.ReadLine();
+
+            int svar = 0;
+            for (int i = 0; i < heltal.Length; i++)
+            {
+                string tecken = heltal[i].ToString();
+                int siffra = int.Parse(tecken);
+                if (siffra == 9)
+                {
+                    Console.Write("0");
+                }
+                else
+                {
+                    Console.Write(siffra + 1);
+                }
+            }
+        }
+    }
+}
